a088e68d0152f6d166d29d4b25acf74e41757b7d
submergent <133404838+submergent@users.noreply.github.com>
2025-05-20 12:18:18 -0400

    Create xterms.sh
    initial creation

---
+++ xterms.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# THE xterms script i've been using since the 90's
+#
+# to see available colors, use:
+# showrgb | more
+
+
+xterm -sb -sl 1000 -fg white -bg darkorchid -title darkorchid&
+xterm -sb -sl 1000 -fg white -bg red4 -title red&
+xterm -sb -sl 1000 -fg white -bg seagreen -title green&
+xterm -sb -sl 1000 -fg white -bg blue3 -title blue&
+xterm -sb -sl 1000 -fg white -bg cornflowerblue -title cornflowerBlue&
+xterm -sb -sl 1000 -fg white -bg cadetblue -title cadetBlue&
+xterm -sb -sl 1000 -fg black -bg orange -title orange&
+xterm -sb -sl 1000 -fg black -bg yellow2 -title yellow&
+xterm -sb -sl 1000 -fg black -bg red
+xterm -sb -sl 1000 -fg MediumOrchid4 -bg salmon1&
+xterm -sb -sl 1000 -fg white -bg SteelBlue4 &
+xterm -sb -sl 1000 -fg white -bg turquoise4&
+xterm -sb -sl 1000 -fg black -bg chartreuse1&
+xterm -sb -sl 1000 -fg black -bg chocolate3&
